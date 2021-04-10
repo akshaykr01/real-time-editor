374370af63026f644c6e415fb8691bbc0190c991
akshay <studtheakshay007@gmail.com>
2021-04-10 14:26:17 +0530

    realTimeEditor

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express')
+    , app = express()
+    , server = require('http').Server(app)
+    , io = require('socket.io')(server)
+    , bodyParser = require('body-parser');
+
+app.use( bodyParser.json());    // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
+app.set('view engine', 'ejs');
+app.use('/front', express.static('front'));
+app.get('/', function (req, res) {
+    res.render("index");
+});
+
+var room_last_content = {};
+io.on('connection', function (socket) {
+    var sock_room;
+    socket.on('doc_name', function(data){
+        sock_room = data.name;
+        socket.join(data.name);
+        socket.emit('set_update', room_last_content[sock_room]);
+    });
+
+    socket.on('update_text', function (data) {
+        room_last_content[sock_room] = data;
+        io.to(sock_room).emit('set_update', data);
+    });
+});
+
+var s = server.listen(3000, function () {
+    console.log('Server is started at 3000');
+});
